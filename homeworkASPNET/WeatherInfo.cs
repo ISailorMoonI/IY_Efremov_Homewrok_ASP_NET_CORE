d9c16120511e628e4ced91b81a4cd99c8dd18cec
SailorMoon <yashamaru99@gmail.com>
2021-04-28 06:03:17 +0300

    HW1 work in progress

---
+++ homeworkASPNET/WeatherInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homeworkASPNET
+{
+    public class WeatherInfo
+    {
+        public int Temperature { get; set; }
+        public DateTime Date { get; set; }
+
+        private List<WeatherInfo> info = new List<WeatherInfo>();
+    }
+}
