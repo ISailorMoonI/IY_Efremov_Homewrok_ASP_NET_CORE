cda22201a090c515cb25b2084bab80114becd7f6
SailorMoon <yashamaru99@gmail.com>
2021-04-28 06:04:45 +0300

    test

--- homeworkASPNET/WeatherInfo.cs
+++ homeworkASPNET/WeatherInfo.cs
@@ -4,12 +4,4 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace homeworkASPNET
-{
-    public class WeatherInfo
-    {
-        public int Temperature { get; set; }
-        public DateTime Date { get; set; }
 
-        private List<WeatherInfo> info = new List<WeatherInfo>();
-    }
-}
