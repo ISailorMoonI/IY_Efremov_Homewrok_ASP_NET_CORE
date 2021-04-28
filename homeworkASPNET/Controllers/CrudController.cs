b802f471787e16ba3230c3db423bb9fba5acdf6c
SailorMoon <yashamaru99@gmail.com>
2021-04-28 06:05:39 +0300

    HW1 in progress

--- homeworkASPNET/Controllers/CrudController.cs
+++ homeworkASPNET/Controllers/CrudController.cs
@@ -5,4 +5,42 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace homeworkASPNET.Controllers
+{
+    [Route("api/crud")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private readonly List<WeatherInfo> _info = new List<WeatherInfo>();
 
+        public CrudController(WeatherInfo info) => this._info = info;
+
+
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] int Temperature)
+        {
+            _info.Add(Temperature);
+            return Ok();
+        }
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(_info);
+        }
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] int Temperature, DateTime Date, [FromQuery] int newTemperature)
+        {
+            for(int i=0; i<_info.Count; i++)
+            {
+                if (_info[i] == Temperature)
+                    _info[i] = newTemperature;
+            }
+            return Ok();
+        }
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string stringsToDelete)
+        {
+            holder.Values = holder.Values.Where(w => w != stringsToDelete).ToList();
+            return Ok();
+        }
+    }
+}
--- homeworkASPNET/WeatherInfo.cs
+++ homeworkASPNET/WeatherInfo.cs
@@ -4,4 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace homeworkASPNET
+{
+    public class WeatherInfo
+    {
+        public int Temperature { get; set; }
+        public DateTime Date { get; set; }
 
+        private List<WeatherInfo> info = new List<WeatherInfo>();
+    }
+}
